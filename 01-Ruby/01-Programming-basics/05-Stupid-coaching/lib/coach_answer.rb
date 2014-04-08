fb5a8514f3ea6316fded2ab12cceff7395dd158f
Nicolas T <ntournaud@gmail.com>
2014-04-08 16:23:36 +0200

    First attempt, solved it

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,5 +1,15 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+
+   if your_message == "I am going to work right now SIR !"
+		coach_answer = ""
+
+  	elsif your_message.include?("?")
+  		coach_answer = "Silly question, get dressed and go to work !"
+
+  	else
+  		coach_answer = "I don't care son, get dressed and go to work !"
+  	end
+
 end
 
 def coach_answer_enhanced(your_message)
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,15 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+### Talk to your coach! ###
+puts 'Ask something to your coach'
+input_to_your_coach = gets.chomp
+
+# Fonction
+coach_answer = coach_answer(input_to_your_coach)
+
+
+# Finally, print user's age in days:
+puts "#{coach_answer}"
+
+
