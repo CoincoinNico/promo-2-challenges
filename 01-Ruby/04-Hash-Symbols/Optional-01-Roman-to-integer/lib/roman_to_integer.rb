14b18547985f4f9821703a7a59f7c1e360e40291
Nicolas T <ntournaud@gmail.com>
2014-04-11 14:59:39 +0200

    First attempt

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,9 +1,33 @@
+TABLE_CONVERSION = {
+  "I" => 1,
+  "V" => 5,
+  "X" => 10,
+  "L" => 50,
+  "C" => 100,
+  "D" => 500,
+  "M" => 1000
+}
+
+
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+  sum = 0
+  romans = roman_string.split(//)
+  for i in 0...romans.length-1 do
+    if TABLE_CONVERSION[romans[i]] >= TABLE_CONVERSION[romans[i+1]]
+      sum += TABLE_CONVERSION[romans[i]]
+    else
+      sum -= TABLE_CONVERSION[romans[i]]
+    end
+  end
+
+  sum += TABLE_CONVERSION[romans[romans.length-1]]
+  sum
+
 end
 
 # testing your program
+roman_to_integer("XII") == 12 # => true
 puts roman_to_integer('IV') == 4 # => true
-puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
 
