dd0f6f57075b84c133e214c78348af19c0b912dc
Nicolas T <ntournaud@gmail.com>
2014-04-14 13:23:09 +0200

    First attempt

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -2,4 +2,19 @@
 
 def mail_joke(email)
   # TODO: Return a joke suited to the email provided
+  name = email.match(/^(\w+|\.|_|-)+@+(\w+\.+\w{1,4})/)[1]
+  provider = email.match(/^(\w+|\.|_|-)+@+(\w+\.+\w{1,4})/)[2]
+
+    raise ArgumentError unless email.match(/^(\w+|\.|_|-)+@+(\w+\.+\w{1,4})/)
+    if provider.include? "lewagon.org"
+      "Well done #{name}, you're skilled and capable"
+    elsif provider.include? "gmail.com"
+      "#{name}, you're an average but modern person"
+    elsif provider.include? "live.com"
+      "#{name}, aren't you born before 1973?"
+    else
+      "Sorry #{name}, we don't know how to judge '#{provider}'"
+    end
+
 end
+
