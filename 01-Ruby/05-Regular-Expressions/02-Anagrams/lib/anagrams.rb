289369327efc220ed24ef36e3d8ff3d0d4363cd0
Nicolas T <ntournaud@gmail.com>
2014-04-14 11:59:01 +0200

    First attempt

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,16 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+  a_string.downcase.gsub(/[\W]/,"").each_char.to_a.sort_by { |letter| letter} == another_string.downcase.gsub(/[\W]/,"").each_char.to_a.sort_by { |letter| letter}
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+  hash = {}
+    hash[a_string.downcase.gsub(/[\W]/,"").each_char.to_a.sort_by { |letter| letter}]  = a_string
+    if  hash[another_string.downcase.gsub(/[\W]/,"").each_char.to_a.sort_by { |letter| letter}]
+      true
+    else
+      false
+    end
+
 end
-  
