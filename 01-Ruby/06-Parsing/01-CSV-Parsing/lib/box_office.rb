1bb5f08610b13fd072cafe44bb8ba459f15e43ee
Nicolas T <ntournaud@gmail.com>
2014-04-15 10:15:37 +0200

    First attempt

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -3,4 +3,20 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+  resultat_int = []
+  CSV.foreach(file_name) do |row|
+  movie = {
+    name: row[0],
+    year: row[1].to_i,
+    earnings: row[2].to_i
+  }
+  resultat_int << movie
+  end
+
+
+  resultat = resultat_int.sort_by { |hsh| hsh[:earnings] }.reverse!.select { |hsh| hsh[:year] < max_year }.take(number)
+
+  resultat
+
 end
+
