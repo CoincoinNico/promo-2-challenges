61df36fda440cf1c80347a8b0c67a7618be5ca58
Nicolas T <ntournaud@gmail.com>
2014-04-16 11:11:35 +0200

    First attempt, solved it

--- 02-OOP/02-Getters-and-Setters/casanova_accessors.rb
+++ 02-OOP/02-Getters-and-Setters/casanova_accessors.rb
@@ -1,35 +1,40 @@
 class Playboy
-  
+
   def initialize(name, nationality)
     @conquests = []
-    @name, @nationality = name, nationality   
-    @married = false 
+    @name, @nationality = name, nationality
+    @married = false
     @status = "single"
   end
-  
+
   def meets(lady)
     @conquests << lady unless @married
   end
-  
+
   def marry(lady)
     @married = true
   end
-  
+
+  attr_reader :nationality
+  attr_reader :conquests
+
+  attr_accessor :status
+
 end
 
-# 1. Set nationality getter or setter 
+# 1. Set nationality getter or setter
 casanova = Playboy.new("Giacomo Casanova", "Italian")
 puts casanova.nationality
 
-# 2. Set conquests getter or setter 
+# 2. Set conquests getter or setter
 casanova.meets("Giullia")
 casanova.meets("Louisia")
 casanova.conquests.each_with_index {|conquest, index| puts "conquest #{index + 1} : #{conquest}"}
 
-# 3. Set status getter or setter 
-# Casanova gets married.. 
+# 3. Set status getter or setter
+# Casanova gets married..
 casanova.marry("Giorgia")
 casanova.status = "married"
 
-# 4. Set status getter or setter 
+# 4. Set status getter or setter
 puts casanova.status
