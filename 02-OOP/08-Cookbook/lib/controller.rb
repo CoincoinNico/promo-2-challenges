d88430a7e981d92830b9bc5e7155a8e543517d7c
Nicolas T <ntournaud@gmail.com>
2014-04-21 01:03:55 +0200

    wtf

--- 02-OOP/08-Cookbook/lib/controller.rb
+++ 02-OOP/08-Cookbook/lib/controller.rb
@@ -3,7 +3,17 @@ require_relative 'cookbook'
 class Controller
   def initialize(file)
     # Here you should instantiate the Cookbook model with the file
+    @file = file
   end
 
   # TODO: Implement the methods to retrieve, add, and delete recipes through the model
+  def list
+  end
+
+  def add
+  end
+
+  def delete
+  end
+
 end
--- 02-OOP/08-Cookbook/lib/cookbook.rb
+++ 02-OOP/08-Cookbook/lib/cookbook.rb
@@ -1,9 +1,50 @@
+#model. Dialogue avec la BDD.
+require "csv"
+
 class Cookbook
+  attr_reader :recipes
   def initialize(file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    @file = file
+    @recipes = []
+    CSV.read(@file).flatten.each { |recipe| @recipes << recipe}
   end
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
+  def all
+    @recipes
+  end
+
+  def create(recipe)
+    CSV.open(@file, "a") { @recipes << recipe }
+  end
+
+  def destroy(index)
+    i = 0
+    CSV.foreach(@file) do |row|
+      @recipes.slice!(index) if i == index
+      i += 1
+    end
+  end
+
   # TODO: Implement a save method that will write the data into the CSV
+
+  def save
+  end
   # And don't forget to use this save method when you have to modify something in your recipes array.
 end
+
+leo = Cookbook.new("lib/recipes.csv")
+p leo.all
+leo.create("Cake")
+p leo.all
+
+array = []
+leo1 = CSV.read("lib/recipes.csv").flatten.each {|line| array << line}
+p leo1
+p array
+leo.create("Cake")
+p array
+
+array.slice!(2)
+p array
