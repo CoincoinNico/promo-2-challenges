3ead678864b2c331d1ea8b2459a6ff65d9d05359
Nicolas T <ntournaud@gmail.com>
2014-04-20 18:30:14 +0200

    self - tricky...

--- 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
@@ -1,14 +1,11 @@
 class Restaurant
   attr_reader :city
-  #TODO add relevant accessors if necessary
 
   def initialize(city, name)
-    #TODO: implement constructor with relevant instance variables
     @city, @name = city, name
     @average_array = []
   end
 
-  #TODO: implement #filter_by_city and #rate methods
   def rate(rating)
     @average_array << rating.to_f
   end
