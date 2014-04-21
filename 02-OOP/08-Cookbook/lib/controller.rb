54e538d03bc17ff4437f5422b6ecad17e0ea51e9
Nicolas T <ntournaud@gmail.com>
2014-04-21 02:05:21 +0200

    not useful instance variable in controller

--- 02-OOP/08-Cookbook/lib/controller.rb
+++ 02-OOP/08-Cookbook/lib/controller.rb
@@ -3,8 +3,7 @@ require_relative 'cookbook'
 class Controller
   def initialize(file)
     # Here you should instantiate the Cookbook model with the file
-    @file = file
-    @cookbook = Cookbook.new(@file)
+    @cookbook = Cookbook.new(file)
   end
 
   # TODO: Implement the methods to retrieve, add, and delete recipes through the model
