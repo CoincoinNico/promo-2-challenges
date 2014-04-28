9f632c40ae1800a9a7f2ac857bb1e57b48eec7db
Nicolas T <ntournaud@gmail.com>
2014-04-28 15:18:30 +0200

    Association

--- 03-AR-Database/08-Hackernews-associations/lib/interface.rb
+++ 03-AR-Database/08-Hackernews-associations/lib/interface.rb
@@ -15,6 +15,9 @@ while true
   until logged_in
     puts "Please login with your <id>"
     #TODO: instantiate a user with his <id>
+    id = gets.chomp
+    logged_in = true if User.find_by(id: id)
+    user = User.find_by(id: id)
   end
 
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
@@ -22,9 +25,9 @@ while true
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
-  
+
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
@@ -32,12 +35,16 @@ while true
     rating = ask_and_get("rating")
     post = { name: name, source_url: source_url, date: Time.now, rating: rating }
     #TODO: use ActiveRecord to add a new post for the user logged in!
+    user.posts.create(post)
   when 2
-    #TODO: use ActiveRecord to get all posts of the current user
+    user.posts.all.each do |post|
+      p "#{post.id}. #{post.name}. URL: #{post.source_url}, RATING: #{post.rating} ADDED ON: #{post.date} BELONGS TO ID #{post.user_id}"
+    end
   when 3
-    #TODO: use ActiveRecord to delete all posts of current user 
-  when 4 
+    #TODO: use ActiveRecord to delete all posts of current user
+    user.posts.delete_all
+  when 4
     break
-	end 
-	
+	end
+
 end
