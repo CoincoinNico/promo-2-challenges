2a744ef8c83f269c167eab04bd0658c6c34ef2bb
Nicolas T <ntournaud@gmail.com>
2014-05-06 09:34:39 +0200

    class blue added

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,20 +8,20 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = "boris@lewagon.org";  // TODO: replace null, keep the email variable.
+  var email = $('#email').val();  // TODO: replace null, keep the email variable.
 
 
 
   // Ex 2. Fill the content of the email input with your email
-
-
+  $('#email').attr('value', 'ntournaud@gmail.com');
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
+  $('email-hint').html('<strong>This is my email now</strong>');
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  $('.table > thead > tr > th').addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
