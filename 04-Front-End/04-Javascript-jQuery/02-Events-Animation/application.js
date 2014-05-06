2d838a85d17d26614781b1fea098be78cca3a4fc
Nicolas T <ntournaud@gmail.com>
2014-05-06 10:22:29 +0200

    commented correction at Le Wagon

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,27 @@
   * TODO: add your code here!
   *
 */
+
+/* hide message id before making it appear (slideDown is an appearance effect) */
+$('#message').hide();
+
+/* delay when document is ready by 500 and add effect */
+$(document).ready(function() {
+  $('#message').delay(500).slideDown(1000);
+});
+
+/* sur un click remonte */
+$('#message').on('click', function(e) {
+  $(this).slideUp();
+});
+
+/* message isn't here anymore but message-wrapper is, so we make it appear*/
+$('#message-wrapper').on('mouseenter', function(e) {
+  $('#message').slideDown();
+});
+
+/* and leave when mouse leaves */
+
+$('#message-wrapper').on('mouseleave', function(e) {
+  $('#message').slideUp();
+});
