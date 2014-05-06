65de9f31fe2c6f2fab6ee3cf949e50bb2b63ec40
Nicolas T <ntournaud@gmail.com>
2014-05-06 09:44:38 +0200

    Added cherbourg row

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -17,7 +17,7 @@ function run_challenges() {
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-  $('email-hint').html('<strong>This is my email now</strong>');
+  $('#email-hint').html('<strong>This is my email now</strong>');
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
@@ -25,13 +25,11 @@ function run_challenges() {
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = $('tbody > tr').size();  // TODO: replace 0, keep the team_count variable.
-
-
+  var team_count = $('tbody > tr').length;  // TODO: replace 0, keep the team_count variable.
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+  $('.table tbody').append('<tr><td>15</td><td>Cherbourg Olympique </td><td>27</td></tr>');
 
 
   // Ex 7. Write some code to sum all points given to all teams
